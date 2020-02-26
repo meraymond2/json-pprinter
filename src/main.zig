6f7544f9a0f20241c9572a7f7cc0fec02f920839
Michael Raymond <raymond.michael@gmail.com>
2020-02-26 19:41:16 +0000

    wip

--- src/main.zig
+++ src/main.zig
@@ -1,42 +1,122 @@
 const std = @import("std");
-const io = std.io;
-const ArrayList = std.ArrayList;
-const scan = @import("scan.zig").scan;
-const pprint = @import("print.zig").pprint;
 
-pub fn main() !void {
-    // Uncomment this when I can get the arena allocator to stop seg-faulting :(
-    // var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    // defer arena.deinit();
-    // var allocator = arena.allocator;
-    var allocator = std.heap.page_allocator;
+const BUFFER_LENGTH = 1024;
 
-    // For now, keep it all in memory. Maybe later,
-    // think about streaming it.
+const TokenTag = enum {
+    START,
+    LBRACE,
+    RBRACE,
+    LBRACKET,
+    RBRACKET,
+    COLON,
+    COMMA,
+};
 
-    // Initialise a buffer, length pulled out of a hat.
-    var a: [5000]u8 = undefined;
-    var buf = a[0..a.len];
+pub const Token = union(TokenTag) {
+    START: void,
+    LBRACE: void,
+    RBRACE: void,
+    LBRACKET: void,
+    RBRACKET: void,
+    COLON: void,
+    COMMA: void,
+};
+
+
+pub fn main() void {
+    // TODO, check for a file-path argument, and read that to stream if present.
 
     // Get the stdin as a stream.
     var stdin = std.io.getStdIn().inStream().stream;
 
+    scan(&stdin);
+}
 
-    var bytes_read = try stdin.read(buf);
-    const first_chunk_slice = try allocator.alloc(u8, bytes_read);
-    std.mem.copy(u8, first_chunk_slice, buf[0..bytes_read]);
+fn scan(stream: &InStream): void {
+    // Pre-allocate one kibibyte.
+    var pre_alloc: [BUFFER_LENGTH]u8 = undefined;
+    var buf = pre_alloc[0..pre_alloc.len];
 
-    // Loop through stream until everything is read.
-    var full_input = buf[0..0];
+    // Pre-allocate one byte for last char of previous buffer.
+    var prev_char: u8 = ' ';
+
+    // Pre-allocate for last token of previous buf.
+    var prev_token: Token = Token{ .START = undefined };
+
+    // Keep track of indent across buffers. (Would prefer to keep this contained
+    // in the print function, see below.)
+    var indent: usize = 0;
+
+    // Loop through stream.
     while (bytes_read > 0) : ({ bytes_read = try stdin.read(buf); }) {
-        full_input = try std.fmt.allocPrint(allocator, "{}{}", .{full_input, buf[0..bytes_read]});
+        // TODO: I'd prefer to return a stream rather than nest printing under
+        // scanning, if that's possible.
+        const last_token = scanBuf(buf, prev, current_token, &indent);
+        prev = buf[BUFFER_LENGTH - 1];
+        prev_token = last_token;
     }
 
-    // Scan
-    const tokens = try scan(allocator, full_input);
+    // End with newline.
+    std.debug.warn("\n");
+}
 
-    // Print
-    pprint(tokens.toSlice());
+fn scanBuf(buf: []u8, prev_char: u8, prev_token: Token, indent: *usize) void {
+    const end = BUFFER_LENGTH;
+    var pos: usize = 0;
+    var start: usize = undefined;
+    var current_token: Token = undefined;
 
-    // Profit $$
+    while (pos != end) {
+        const c = buf[pos];
+        switch(c) {
+            '}' => {
+                current_token = Token{ .RBRACE = undefined };
+                printToken(current_token, &indent);
+                pos += 1;
+            },
+            ']' => {
+                current_token = Token{ .RBRACKET = undefined };
+                printToken(current_token, &indent);
+                pos += 1;
+            },
+            ':' => {
+                current_token = Token{ .COLON = undefined }
+                printToken(current_token, &indent);
+                pos += 1;
+            },
+            ',' => {
+                current_token = Token{ .COMMA = undefined };
+                printToken(current_token, &indent);
+                pos += 1;
+            },
+            else => {
+                pos +=1;
+            }
+        }
+    }
+
+    return current_token;
+}
+
+fn printToken(token: Token, indent: *usize) void {
+    switch(token) {
+        START => void,
+        Token.COLON => std.debug.warn(": ", .{}),
+        Token.COMMA => {
+            std.debug.warn(",\n", .{});
+            printIndent(indent);
+        }
+         else => {
+                std.debug.warn("", .{});
+            },
+    }
+}
+
+fn printIndent(n: usize) void {
+    var i = n;
+    while (i > 0) : ({
+        i -= 1;
+    }) {
+        std.debug.warn(tab, .{});
+    }
 }

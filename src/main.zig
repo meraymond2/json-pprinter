bad2f87cb89d5534667315c98b8f3d4b416dd371
Michael Raymond <raymond.michael@gmail.com>
2020-02-26 20:18:10 +0000

    Fix syntax issues

--- src/main.zig
+++ src/main.zig
@@ -22,17 +22,16 @@ pub const Token = union(TokenTag) {
     COMMA: void,
 };
 
-
-pub fn main() void {
+pub fn main() !void {
     // TODO, check for a file-path argument, and read that to stream if present.
 
     // Get the stdin as a stream.
     var stdin = std.io.getStdIn().inStream().stream;
 
-    scan(&stdin);
+    try scan(&stdin);
 }
 
-fn scan(stream: &InStream): void {
+fn scan(stream: *std.io.InStream(std.os.ReadError)) !void {
     // Pre-allocate one kibibyte.
     var pre_alloc: [BUFFER_LENGTH]u8 = undefined;
     var buf = pre_alloc[0..pre_alloc.len];
@@ -48,19 +47,23 @@ fn scan(stream: &InStream): void {
     var indent: usize = 0;
 
     // Loop through stream.
-    while (bytes_read > 0) : ({ bytes_read = try stdin.read(buf); }) {
+    var bytes_read: usize = try stream.read(buf);
+    while (bytes_read > 0) : ({
+        bytes_read = try stream.read(buf);
+    }) {
+        std.debug.warn("{}", .{bytes_read});
         // TODO: I'd prefer to return a stream rather than nest printing under
         // scanning, if that's possible.
-        const last_token = scanBuf(buf, prev, current_token, &indent);
-        prev = buf[BUFFER_LENGTH - 1];
+        const last_token = scanBuf(buf, prev_char, prev_token, &indent);
+        prev_char = buf[BUFFER_LENGTH - 1];
         prev_token = last_token;
     }
 
     // End with newline.
-    std.debug.warn("\n");
+    std.debug.warn("\n", .{});
 }
 
-fn scanBuf(buf: []u8, prev_char: u8, prev_token: Token, indent: *usize) void {
+fn scanBuf(buf: []u8, prev_char: u8, prev_token: Token, indent: *usize) Token {
     const end = BUFFER_LENGTH;
     var pos: usize = 0;
     var start: usize = undefined;
@@ -68,30 +71,30 @@ fn scanBuf(buf: []u8, prev_char: u8, prev_token: Token, indent: *usize) void {
 
     while (pos != end) {
         const c = buf[pos];
-        switch(c) {
+        switch (c) {
             '}' => {
                 current_token = Token{ .RBRACE = undefined };
-                printToken(current_token, &indent);
+                printToken(current_token, indent);
                 pos += 1;
             },
             ']' => {
                 current_token = Token{ .RBRACKET = undefined };
-                printToken(current_token, &indent);
+                printToken(current_token, indent);
                 pos += 1;
             },
             ':' => {
-                current_token = Token{ .COLON = undefined }
-                printToken(current_token, &indent);
+                current_token = Token{ .COLON = undefined };
+                printToken(current_token, indent);
                 pos += 1;
             },
             ',' => {
                 current_token = Token{ .COMMA = undefined };
-                printToken(current_token, &indent);
+                printToken(current_token, indent);
                 pos += 1;
             },
             else => {
-                pos +=1;
-            }
+                pos += 1;
+            },
         }
     }
 
@@ -99,20 +102,21 @@ fn scanBuf(buf: []u8, prev_char: u8, prev_token: Token, indent: *usize) void {
 }
 
 fn printToken(token: Token, indent: *usize) void {
-    switch(token) {
-        START => void,
+    switch (token) {
+        Token.START => undefined,
         Token.COLON => std.debug.warn(": ", .{}),
         Token.COMMA => {
             std.debug.warn(",\n", .{});
-            printIndent(indent);
-        }
-         else => {
-                std.debug.warn("", .{});
-            },
+            printIndent(indent.*);
+        },
+        else => {
+            std.debug.warn("", .{});
+        },
     }
 }
 
 fn printIndent(n: usize) void {
+    const tab = "  ";
     var i = n;
     while (i > 0) : ({
         i -= 1;
